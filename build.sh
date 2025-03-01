4f96cf41ccd564a425b175ecc54fa9c7a4d15b62
bengris32 <bengris32@protonmail.ch>
2025-03-01 07:21:50 +0000

    ARM64: configs: Enable oplus_custom driver

--- build.sh
+++ build.sh
@@ -1,77 +1,48 @@
 #!/bin/bash
-clear
-function compile() 
+function compile()
 {
-echo ORIGIN KERNEL FOR ONEPLUS NORD CE 2 5G, CODENAMED - IVAN
-echo
 source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
-ccache -M 100G >/dev/null
+ccache -M 25G
+TANGGAL=$(date +"%Y%m%d-%H")
 export ARCH=arm64
-export KBUILD_BUILD_HOST=origin
-export KBUILD_BUILD_USER=
+export KBUILD_BUILD_HOST=android-build
+export KBUILD_BUILD_USER="ubuntu"
 clangbin=clang/bin/clang
-if ! [ -a $clangbin ]; then git clone --depth=1 https://github.com/techyminati/android_prebuilts_clang_host_linux-x86_clang-6443078 clang
+if ! [ -a $clangbin ]; then git clone --depth=1 https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-6443078 clang
 fi
-gcc64bin=los-4.9-64/bin/aarch64-linux-android-as
-if ! [ -a $gcc64bin ]; then git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
+gcc64bin=gcc64/bin/aarch64-linux-android-as
+if ! [ -a $gcc64bin ]; then git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 gcc64
 fi
-gcc32bin=los-4.9-32/bin/arm-linux-androideabi-as
-if ! [ -a $gcc32bin ]; then git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
+gcc32bin=gcc32/bin/arm-linux-androideabi-as
+if ! [ -a $gcc32bin ]; then git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 gcc32
 fi
-read -p "Wanna do dirty build? (Y/N): " build_type
-if [[ $build_type == "N" || $build_type == "n" ]]; then
-echo Deleting out directory and doing clean Build
-rm -rf out && mkdir -p out
-fi
-if [[ $build_type == "Y" || $build_type == "y" ]]; then
-echo Warning :- Doing dirty build
-fi
-if ! [[ $build_type == "Y" || $build_type == "y" ]]; then
-if ! [[ $build_type == "N" || $build_type == "n" ]]; then
-echo Invalid Input , Read carefully before typing
-echo Trying to restart script
-. build.sh && exit
-fi
-fi
-
-make O=out ARCH=arm64 k6877v1_64_k419_defconfig
-
-PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
-make -j$(nproc --all)   O=out \
-                        ARCH=arm64 \
-                        CC="clang" \
-                        CLANG_TRIPLE=aarch64-linux-gnu- \
-                        CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-android-" \
-                        CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-androideabi-" \
-                        LD=ld.lld \
-                        AS=llvm-as \
-                        AR=llvm-ar \
-                        NM=llvm-nm \
-                        LLVM_IAS=1 \
-                        OBJCOPY=llvm-objcopy \
-                        CONFIG_NO_ERROR_ON_MISMATCH=y
+rm -rf AnyKernel
+make O=out ARCH=arm64 ivan_defconfig
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/gcc32/bin:${PATH}:${PWD}/gcc64/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE="${PWD}/gcc64/bin/aarch64-linux-android-" \
+                      CROSS_COMPILE_ARM32="${PWD}/gcc32/bin/arm-linux-androideabi-" \
+                      LD=ld.lld \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y
 }
-
 function zupload()
 {
-zimage=out/arch/arm64/boot/Image.gz-dtb
+zimage=out/arch/arm64/boot/Image.gz
 if ! [ -a $zimage ];
 then
-echo  " Failed to compile zImage, fix the errors first "
+echo  " Failed To Compile Kernel"
 else
-echo -e " Build succesful, generating flashable zip now "
-rm -rf AnyKernel
-git clone --depth=1 https://github.com/notganesh/AnyKernel3  AnyKernel
-cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+echo -e " Kernel Compile Successful"
+git clone --depth=1 https://github.com/notganesh/AnyKernel3.git AnyKernel
+cp out/arch/arm64/boot/Image.gz AnyKernel
 cd AnyKernel
-zip -r9 ORIGIN-OSS-KERNEL-MT6877.zip *
-#curl --upload-file ORIGIN-OSS-KERNEL-MT6877.zip https://transfer.sh/
-curl -sL https://git.io/file-transfer | sh
-./transfer anon ORIGIN-OSS-KERNEL-MT6877.zip
+zip -r9 LineagePlus-ivan-${TANGGAL}.zip *
 cd ../
 fi
 }
-
 compile
 zupload
